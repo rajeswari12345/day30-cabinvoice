142e8f404e38619615ae0e1e88cf9c570f06afb6
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-06-19 12:14:51 +0530

    program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+namespace CabInvoiceGenerator
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab  Invoice generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(2, 0, 5);
+            Console.WriteLine($"Fare: {fare}"):
+}
+    }
+}
